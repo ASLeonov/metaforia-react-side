690c846b6d098f850cf717383f877a1d201f90ea
Alex_L <asleonov@inbox.ru>
2020-06-28 15:26:48 +0300

    23-th commit

--- server/websocket.js
+++ server/websocket.js
@@ -1,9 +1,7 @@
-// const express = require('express')
-// const mysql = require('mysql2')
+const mysql = require('mysql2')
 const WebSocket = require('ws')
 const config = require('config')
 
-// const app = express()
 const port = process.env.PORT || 8080
 const wss = new WebSocket.Server({ port: port })
 const dbConfig = config.get('dbConfig')
@@ -15,24 +13,22 @@ console.log('port ws =', port)
 wss.on('connection', function connection(ws) {
 
   ws.on('message', function incoming(message) {
-    // const client_data = JSON.parse(message)
-    // const connection = mysql.createConnection(dbConfig)
-    console.log('recieved msg')
+    const client_data = JSON.parse(message)
+    const connection = mysql.createConnection(dbConfig)
+    // console.log('recieved msg')
     let timerId = setTimeout(function tick() {
-      // const query = `
-      //   SELECT \`last_version\` 
-      //   FROM \`${client_data.user}__sessions\` 
-      //   WHERE \`session_id\` = ${client_data.session}`
-      // connection.query(
-      //   query,
-      //   (err, results, fields) => {
-      //     ws.send(results[0].last_version)  // res.status(status).json(results)
-      //   }
-      // )
-      console.log('send msg')
+      const query = `
+        SELECT \`last_version\` 
+        FROM \`${client_data.user}__sessions\` 
+        WHERE \`session_id\` = ${client_data.session}`
+      connection.query(
+        query,
+        (err, results, fields) => {
+          ws.send(results[0].last_version)  // res.status(status).json(results)
+        }
+      )
+      // console.log('send msg')
       timerId = setTimeout(tick, 7000)
     }, 7000)
   })
 })
-
-// app.listen(port, () => console.log(`Server listening on port ${port}`))
